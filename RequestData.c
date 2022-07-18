fbf7811056c5c5787413f5332cce84e56cd8e4e0
Jose Luis Herrera <herrera.clip@gmail.com>
2022-07-18 02:38:26 -0500

    RequestData crea un archivi .txt

--- RequestData.c
+++ RequestData.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
+#include<conio.h>
+#include<stdlib.h>
 
-main(){
+main()
+{
+    char name[50];
+    
 
+    FILE *arch=fopen("datos1.txt","wt");
+    if (arch==NULL){
+        exit(1);
+    }
+    fputs("Primer linea\n",arch);
+    fputs("Segunda linea\n",arch);
+    fputs("Tercer linea\n",arch);
+    fclose(arch);
+    printf("Se creo el archivo de texto con tres lineas de texto.");
+    getch();
+    return 0;
 }
