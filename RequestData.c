4aaeb808bed606c369fd969b728c2bf54adb0d48
Jose Luis Herrera <herrera.clip@gmail.com>
2022-07-21 03:30:35 -0500

    creacion variables locales para poder liberar la memoria

--- RequestData.c
+++ RequestData.c
@@ -2,7 +2,11 @@
 #include <conio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "lib.h"
+
+void *PassWord (void);
+char *NombrePersona (void);
+char *name;
+char *clave;
 
 main(){
     char *nombre;
@@ -20,5 +24,47 @@ main(){
     fprintf(f, "%s\n", nombre);
     fclose(f);
     free(nombre);
+    free(name);
+    free(clave);
     return (0);
+}
+
+char *NombrePersona (void){
+    char *name,ch;
+    int size=0;
+    name = (char *)calloc(0,1);
+
+    printf("Ingrse su nombre(s) y apellido(s): ");
+
+    while ((ch = getchar()) != '\n' && ch != EOF){
+        name[size]= ch;
+        size++;
+        name = (char *)realloc(name, size);
+    }
+    return name;
+} 
+
+void *PassWord (void){
+    char *clave,ch;
+    int size=0;
+    clave = (char *)calloc(0,1);
+
+    printf("Ingrse su contrasela con caracteres de la A a la Z\n");
+    printf("De 5 a 7 caracteres: ");
+
+    while (ch = getch()){
+        if(ch == 13){
+            break;
+        }
+        if ((ch >= 65 && ch <= 90)||(ch >= 97 && ch <= 122)){
+            clave[size]= ch;
+            printf("*");
+            size++;
+            clave = (char *)realloc(clave, size);
+        }
+        if (size == 7){
+            break;
+        }
+    }
+    return clave;
 }
